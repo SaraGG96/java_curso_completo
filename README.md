743c3c6fc54a410742d343d146d98d773b030573
Sara <saragarciagarrido96@gmail.com>
2026-03-11 19:50:39 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Curso de Java de MoureDev
+
+1.— [Hello World!](src/HelloWorld.java)
---
+++ src/HelloWorldExercises.java
@@ -0,0 +1,34 @@
+/* 
+Clase 14 - Ejercicios: Primeros pasos
+Vídeo: https://youtu.be/JOAqpdM36wI?t=5108
+*/
+
+public class HelloWorldExercises {
+
+    public static void main(String[] args) {
+
+        // 1. Imprime un mensaje que diga tu nombre en lugar de "¡Hola Mundo!".
+        System.out.println("Sara");
+
+        // 2. Imprime dos líneas: "Hola" y luego "Mundo" con un solo println.
+        System.out.println("Hola\nMundo");
+
+        // 5. Imprime tu edad, tu color favorito y tu ciudad.
+        System.out.println("29, Rosa, Vigo");
+
+        // 6. Explora los diferentes System.XXX.println(); más allá de "out".
+        System.err.println("Esto se imprime por consola de error");
+
+        // 7. Utiliza varios println para imprimir una frase.
+        System.out.println("Hola");
+        System.out.println("Mundo");
+
+        // 8. Imprime un diseño ASCII (por ejemplo, una cara feliz usando símbolos).
+        System.out.print("🤖");
+
+        //10. Intenta cambiar el nombre del archivo a uno diferente del de la clase y compílalo. ¿Qué pasa?
+        // "The public type HelloWorldExercises must be defined in its own file"
+
+
+    }
+}
